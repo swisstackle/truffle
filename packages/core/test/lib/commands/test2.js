1d1ca19f76ba134f0749a09e9f1d77c82e750ce6
Alain Schaerer <alainsch@buffalo.edu>
2022-09-15 15:12:09 -0400

    Update test2.js

--- packages/core/test/lib/commands/test2.js
+++ packages/core/test/lib/commands/test2.js
@@ -25,10 +25,129 @@ function updateFile(filename) {
 describe("test command", () => {
     before(function () {
         tempDir = tmp.dirSync({ unsafeCleanup: true });
-        fse.copySync(path.join(__dirname, "../../sources/metacoin"), tempDir.name);
         config = new Config(undefined, tempDir.name);
     });
 
+    it("1 directory with files in it.", async () => {
+        // This allows to create customized directory structure to test more than one level of sub directories.
+        let fileStructrure = {
+            name: "sub_directory",
+            files: [""],
+            subdirs: [
+                {
+                    name: "sub_sub_directory",
+                    files: ["test1.sol", "test2.js", "test3.sol"]
+                }
+            ]
+        };
+
+        // Call method used by Test to discover existing test files. Then create subdirectories and test files in these
+        // subdirectories. Then run Test method again and check if number discovered increased.
+        let testFiles = determineTestFilesToRun({ config });
+        const testFilesCount = testFiles.length;
+
+        const newTestFiles = createTestSubDir(
+            config.test_directory,
+            fileStructrure
+        );
+        console.log(config.test_directory);
+        testFiles = determineTestFilesToRun({ config });
+        assert.equal(
+            testFiles.length,
+            testFilesCount + newTestFiles,
+            "Wrong number of files discovered."
+        );
+    });
+
+    it("2 directories with files in it.", async () => {
+        // This allows to create customized directory structure to test more than one level of sub directories.
+        let fileStructrure = {
+            name: "sub_directory",
+            files: [""],
+            subdirs: [
+                {
+                    name: "sub_sub_directory",
+                    files: ["test1.sol", "test2.js", "test3.sol"]
+                },
+                {
+                    name: "sub_sub_directory2",
+                    files: ["test4.sol", "test5.js", "test6.sol"]
+                }
+            ]
+        };
+
+        // Call method used by Test to discover existing test files. Then create subdirectories and test files in these
+        // subdirectories. Then run Test method again and check if number discovered increased.
+        let testFiles = determineTestFilesToRun({ config });
+        const testFilesCount = testFiles.length;
+
+        const newTestFiles = createTestSubDir(
+            config.test_directory,
+            fileStructrure
+        );
+        console.log(config.test_directory);
+        testFiles = determineTestFilesToRun({ config });
+        assert.equal(
+            testFiles.length,
+            testFilesCount + newTestFiles,
+            "Wrong number of files discovered."
+        );
+    });
+
+    it("3 directories with files in it.", async () => {
+        // This allows to create customized directory structure to test more than one level of sub directories.
+        let fileStructrure = {
+            name: "sub_directory",
+            files: [""],
+            subdirs: [
+                {
+                    name: "sub_sub_directory",
+                    files: ["test1.sol", "test2.js", "test3.sol"]
+                },
+                {
+                    name: "sub_sub_directory2",
+                    files: ["test4.sol", "test5.js", "test6.sol"]
+                },
+                {
+                    name: "sub_sub_directory3",
+                    files: ["test4.sol", "test5.js", "test6.sol"]
+                }
+            ]
+        };
+        // Call method used by Test to discover existing test files. Then create subdirectories and test files in these
+        // subdirectories. Then run Test method again and check if number discovered increased.
+        let testFiles = determineTestFilesToRun({ config });
+        const testFilesCount = testFiles.length;
+
+        const newTestFiles = createTestSubDir(
+            config.test_directory,
+            fileStructrure
+        );
+        console.log(config.test_directory);
+        testFiles = determineTestFilesToRun({ config });
+        assert.equal(
+            testFiles.length,
+            testFilesCount + newTestFiles,
+            "Wrong number of files discovered."
+        );
+    });
+
+    it("1 directory with no files in it.", async () => {
+        tempDir = tmp.dirSync({ unsafeCleanup: true });
+        config = new Config(undefined, tempDir.name);
+        let dirName = path.join(
+            config.test_directory,
+            "sub_directory"
+        );
+        fse.ensureDirSync(dirName);
+        console.log(config.test_directory);
+        testFiles = determineTestFilesToRun({ config });
+        assert.equal(
+            testFiles.length,
+            0,
+            "Wrong number of files discovered."
+        );
+    });
     //  Test with cycles and no missing directories, where all directories exist and all directories contain one or more files. Also, one file is outside the test directory but linked to it with a symlink inside the test directory.
     it("Check with cycles plus if it follows symlinks outside of the test directory.", async () => {
         // This allows to create customized directory structure to test more than one level of sub directories.
@@ -108,86 +227,16 @@ describe("test command", () => {
             ]
         };
 
-        // Create file in recursion. Used instead foreach since foreach loop doesn't wait for files to be created
-        // Function does not count already existing files.
-        function createFile(dirName, files, index) {
-            // return zero if there are no files to create
-            if (!files[index]) return 0;
-            var fileName = path.join(dirName, files[index]);
-            let filesCount = 0;
-            if (!fse.existsSync(fileName)) {
-                fse.createFileSync(fileName);
-                filesCount++;
-            }
-            if (files.length > index + 1) {
-                filesCount += createFile(dirName, files, index + 1);
-            }
-
-            return filesCount;
-        }
-
-        function createSymlinks(symlinks) {
-            console.log(
-                "Creating symlink: ",
-                path.join(config.test_directory, "symlink"),
-                " to ",
-                path.join(config.test_directory, "testsub")
-            );
-            fs.symlink(
-                symlinks.destination,
-                path.join(symlinks.src, symlinks.name),
-                err => {
-                    console.log(err);
-                }
-            );
-        }
-
-        function createAFile(dirName, file) {
-            var fileName = path.join(dirName, file);
-            if (!fse.existsSync(fileName)) {
-                fs.writeFileSync( fileName, "");
-            }
-        }
-
-        // Create files using dirStruct object. Count newly created files and skip existing files.
-        // Returns number of new files.
-        function createTestSubDir(dirName, dirStruct, symlink) {
-            var numOfFiles = 0;
-            if (symlink) {
-                createSymlinks(dirStruct);
-            } else {
-                numOfFiles = createFile(dirName, dirStruct.files, 0);
-            }
-
-            if (dirStruct.subdirs && !symlink) {
-                dirStruct.subdirs.forEach(val => {
-                    numOfFiles += createTestSubDir(
-                        path.join(dirName, dirStruct.name, val.name),
-                        val,
-                        false
-                    );
-                });
-            }
-            if (dirStruct.symlinks) {
-                dirStruct.symlinks.forEach(val => {
-                    console.log(val.destination);
-                    createTestSubDir(val.destination, val, true);
-                });
-            }
-            return numOfFiles;
-        }
         // Call method used by Test to discover existing test files. Then create subdirectories and test files in these
         // subdirectories. Then run Test method again and check if number discovered increased.
         let testFiles = determineTestFilesToRun({ config });
         const testFilesCount = testFiles.length;
-
         const newTestFiles = createTestSubDir(
             config.test_directory,
             fileStructrure
         );
-
-        createAFile(path.join(config.test_directory, ".."), "random.sol");   // Create a file outside of test directory but linked to it with a symlink inside the test directory.
         console.log(config.test_directory);
+        createAFile(path.join(config.test_directory, ".."), "random.sol");   // Create a file outside of test directory but linked to it with a symlink inside the test directory.
         testFiles = determineTestFilesToRun({ config });
         assert.equal(
             testFiles.length,
@@ -198,7 +247,70 @@ describe("test command", () => {
 
     //  Test with no argument and empty directory
     it("No argument and empty directory.", async () => {
+        tempDir = tmp.dirSync({ unsafeCleanup: true });
+        config = new Config(undefined, tempDir.name);
         let testFiles = determineTestFilesToRun({ config });
         assert.equal(testFiles.length, 0, "Non-existing file was detected.");
     });
 }).timeout(1000);
+
+// Create file in recursion. Used instead foreach since foreach loop doesn't wait for files to be created
+// Function does not count already existing files.
+function createFile(dirName, files, index) {
+    // return zero if there are no files to create
+    if (!files[index]) return 0;
+    var fileName = path.join(dirName, files[index]);
+    let filesCount = 0;
+    if (!fse.existsSync(fileName)) {
+        fse.createFileSync(fileName);
+        filesCount++;
+    }
+    if (files.length > index + 1) {
+        filesCount += createFile(dirName, files, index + 1);
+    }
+
+    return filesCount;
+}
+
+function createSymlinks(symlinks) {
+    fs.symlink(
+        symlinks.destination,
+        path.join(symlinks.src, symlinks.name),
+        err => {
+        }
+    );
+}
+
+function createAFile(dirName, file) {
+    var fileName = path.join(dirName, file);
+    if (!fse.existsSync(fileName)) {
+        fs.writeFileSync( fileName, "");
+    }
+}
+
+// Create files using dirStruct object. Count newly created files and skip existing files.
+// Returns number of new files.
+function createTestSubDir(dirName, dirStruct, symlink) {
+    var numOfFiles = 0;
+    if (symlink) {
+        createSymlinks(dirStruct);
+    } else {
+        numOfFiles = createFile(dirName, dirStruct.files, 0);
+    }
+
+    if (dirStruct.subdirs && !symlink) {
+        dirStruct.subdirs.forEach(val => {
+            numOfFiles += createTestSubDir(
+                path.join(dirName, dirStruct.name, val.name),
+                val,
+                false
+            );
+        });
+    }
+    if (dirStruct.symlinks) {
+        dirStruct.symlinks.forEach(val => {
+            createTestSubDir(val.destination, val, true);
+        });
+    }
+    return numOfFiles;
+}
